8fa16e49349f2e80661b394c691f94fc40e3598e
MITHLESH KUMAR <mmk329460@gmail.com>
2026-04-23 21:09:54 +0530

    fix folder structure components

---
+++ Employee/src/components/FooterComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+      <footer className='footers'>
+      <samp>All rights reserved 2026 by mithleshcse</samp>
+      </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
---
+++ Employee/src/components/ListEmployeeComponent.jsx
@@ -0,0 +1,96 @@
+import React, {useEffect, useState} from 'react'
+import { deletEmployee, listEmployee } from '../service/EmployeeService.js'
+import { useNavigate } from 'react-router-dom'
+
+const ListEmployeeComponent = () => {
+
+ const [employee,setEmployee] =useState([])
+ const navigator=useNavigate();
+
+ useEffect(() =>{
+  getAllEmployee();
+ }, [])
+
+ function getAllEmployee(){
+  listEmployee().then((Response) =>{
+    setEmployee(Response.data);
+  }).catch(error => {
+    console.error(error);
+  });
+ }
+
+ function addNewEmployee(){
+    navigator('/add-employee')
+ }
+
+ function updateEmployee(id){
+  navigator(`/edit-employee/${id}`)   
+ }
+
+ function removeEmployee(id){
+  console.log(id);
+
+  deletEmployee(id).then((Response) =>{
+   getAllEmployee();
+  }).catch(error =>{
+   console.error(error);
+  })
+ }
+
+ return (
+  <div className='container'>
+
+      <div className='con' style={{
+        background: 'linear-gradient(135deg, #1a1a2e 0%, #16213e 50%, #0f3460 100%)',
+        position: 'fixed',
+        top: 0, left: 0,
+        width: '100%', height: '100%',
+        zIndex: -1
+      }}></div>
+
+
+    <h2 id='A' className='text-center'>List of Employees</h2>
+
+    <button className='btn btn-primary mb-2' onClick={addNewEmployee}>
+      Add Employee
+    </button>
+
+    <table className='table table-striped table-bordered'>
+      <thead>
+        <tr>
+          <th>Employee Id</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Email</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {
+          employee.map(emp =>
+            <tr key={emp.id}>
+              <td>{emp.id}</td>
+              <td>{emp.firstName}</td>
+              <td>{emp.lastName}</td>
+              <td>{emp.email}</td>
+              <td>
+                <button 
+                  className='btn btn-info'
+                  onClick={()=> updateEmployee(emp.id)}
+                >
+                  Update
+                </button>
+                <button className='btn btn-danger'  style={{marginLeft:'10px'}} onClick={() => removeEmployee(emp.id)}>Delete</button>
+
+              </td>
+            </tr>
+          )
+        }
+      </tbody>
+    </table>
+  </div>
+ )
+}
+
+export default ListEmployeeComponent
